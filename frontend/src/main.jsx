6a646138b7b2e101ae7b158cdd29adf22c657444
workgpt300-art <workgpt300@gmail.com>
2026-04-10 13:41:24 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './i18n' // Підключення мов
+import './index.css' // <--- ДОДАЙ ЦЕЙ РЯДОК (це підключить твої анімації)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
